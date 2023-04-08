5ef98fe92eb2323b2eb8a0e643bce4f16673736b
İBRAHİM OKUR <ibrahimokurrr@hotmail.com>
2023-04-09 01:59:57 +0300

    Movement

---
+++ Assets/Player/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] Movement movement;
+    [SerializeField] Mouse_Look mouseLook;
+    PlayerControls controls;
+    PlayerControls.GroundMovementActions groundMovement;
+    Vector2 horizontalInput;
+    Vector2 mouseInput;
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        groundMovement = controls.GroundMovement;
+        groundMovement.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        groundMovement.Jump.performed += _ => movement.OnJumpPressed();
+        groundMovement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        groundMovement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+    }
+
+    private void Update()
+    {
+        movement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Disable();
+    }
+}
---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+#pragma warning disable 649
+
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 11f;
+    Vector2 horizontalInput;
+
+    [SerializeField] float jumpHeight = 3.5f;
+    bool jump;
+    [SerializeField] float gravity = -30f; //-9.81
+    Vector3 verticalVelocity = Vector3.zero;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundMask);
+        if (isGrounded)
+        {
+            verticalVelocity.y = 0;
+        }
+        
+        Vector3 horizontalVelocity = (transform.right * horizontalInput.x + transform.forward * horizontalInput.y) * speed;
+        controller.Move(horizontalVelocity * Time.deltaTime);
+
+        if (jump)
+        {
+            if (isGrounded)
+            {
+                verticalVelocity.y = Mathf.Sqrt(-2f * jumpHeight * gravity);
+            }
+            jump = false;
+        }
+
+        verticalVelocity.y += gravity * Time.deltaTime;
+        controller.Move(verticalVelocity * Time.deltaTime);
+    }
+    public void ReceiveInput (Vector2 _horizantalInput)
+    {
+        horizontalInput = _horizantalInput;
+        print(horizontalInput);
+    }
+
+    public void OnJumpPressed()
+    {
+        jump = true;
+    }
+
+
+
+}
