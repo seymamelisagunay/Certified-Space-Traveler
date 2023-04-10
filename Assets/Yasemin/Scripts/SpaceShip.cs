ce6c76a3f08ece2ad65ad24ce513f9e868a77128
Yasemin Toraman <yaseminntoraman@gmail.com>
2023-04-10 05:25:48 +0300

    .

--- Assets/Yasemin/Scripts/SpaceShip.cs
+++ Assets/Yasemin/Scripts/SpaceShip.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
 {
-    public float hareketHizi = 5f;
-    public float yanHareketHizi = 2f;
+    public float moveSpeed = 5f;
+    public float horizontalSpeed = 2f;
 
-    private bool yukariMi = true;
+    private bool isUp = true;
 
     private const float maxX = 7.7f;
     private const float minX = -4.4f;
@@ -17,40 +17,40 @@ public class SpaceShip : MonoBehaviour
 
     void Update()
     {
-        if (yukariMi)
+        if (isUp)
         {
             // Gemi yukarý doðru hareket eder
-            transform.Translate(Vector3.up * hareketHizi * Time.deltaTime);
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
 
             // Sað-Sol hareketi için girdi alýnýr
             float horizontal = Input.GetAxis("Horizontal");
 
             // Gemi yatayda hareket eder
-            transform.Translate(Vector3.right * horizontal * yanHareketHizi * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontal * horizontalSpeed * Time.deltaTime);
 
             // Eðer gemi ekranýn üstünden çýkarsa, aþaðýya döndürülür ve hareket yönü tersine çevrilir
             if (transform.position.y >  maxY)
             {
                 transform.Rotate(Vector3.forward, 180f);
-                yukariMi = false;
+                isUp = false;
             }
         }
         else
         {
-            // Gemi aþaðý doðru hareket eder
-            transform.Translate(-Vector3.down * hareketHizi * Time.deltaTime);
+           
+            transform.Translate(-Vector3.down * moveSpeed * Time.deltaTime);
 
             // Sað-Sol hareketi için girdi alýnýr
             float horizontal = Input.GetAxis("Horizontal");
 
             // Gemi yatayda hareket eder
-            transform.Translate(Vector3.right * horizontal * yanHareketHizi * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontal * horizontalSpeed * Time.deltaTime);
 
             // Eðer gemi ekranýn altýndan çýkarsa, yukarýya döndürülür ve hareket yönü tersine çevrilir
             if (transform.position.y < minY)
             {
                 transform.Rotate(Vector3.forward, 180f);
-                yukariMi = true;
+                isUp = true;
             }
         }
     }
